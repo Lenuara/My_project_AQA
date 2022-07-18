03f87347d799723b76d32d7c3db82e829069de80
Lenuara <keypost777@gmail.com>
2022-07-18 21:34:48 +0300

    update elements tests
    added class:
    - TestLinksPage
    added tests:
    - test_check_link
    - test_broken_link

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,10 +1,11 @@
 import random
 import time
 
+import requests as requests
 from selenium.webdriver.common.by import By
 from generator.generator import generated_person
 from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators, RadioButtonPageLocators, \
-    WebTablePageLocators, ButtonsPageLocators
+    WebTablePageLocators, ButtonsPageLocators, LinksPageLocators
 from pages.base_page import BasePage
 
 
@@ -177,6 +178,36 @@ class ButtonsPage(BasePage):
 
     def check_clicked_on_the_button(self, element):
         return self.element_is_presence(element).text
+
+class LinksPage(BasePage):
+
+    locators = LinksPageLocators()
+
+    def check_new_tab_simple_link(self):
+        simple_link = self.element_is_visible(self.locators.SIMPLE_LINK)
+        link_href = simple_link.get_attribute('href')
+        request = requests.get(link_href)
+        # request = requests.get(f"{link_href}bad-request") # ломаем ссылку для проверки
+        if request.status_code == 200:
+           simple_link.click()
+           self.driver.switch_to.window(self.driver.window_handles[1])
+           url = self.driver.current_url
+           return link_href, url
+        else:
+            return link_href, request.status_code
+
+    def check_broken_link(self, url):
+        request = requests.get(url)
+        if request.status_code == 200:
+            self.element_is_presence(self.locators.BAD_REQUEST).click()
+        else:
+            return request.status_code
+
+
+
+
+
+
 
 
 
